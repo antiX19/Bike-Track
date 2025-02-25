a3c28ddcab3778974ff7e8e930e17d51fb2908a5
RyanT934 <turcot.ryan@yahoo.fr>
2025-02-25 12:19:04 +0100

    Update index.php

--- Backend/index.php
+++ Backend/index.php
@@ -7,16 +7,19 @@
  *
  * ## Présentation
  * Le fichier `api.js` met en place une API avec les fonctionnalités suivantes :
- * - Serveur HTTP sur le port 3000 et HTTPS sur le port 3002.
+ * - Serveur HTTP sur le port 3000.
  * - Connexion à une base de données MySQL `biketrack`.
  * - Chargement des certificats SSL pour sécuriser les connexions.
  * - Routes pour interagir avec l'API.
+ * - Génération de token sécurisé
  *
  * ## Utilisation
  *
  * 1. Installer les dépendances avec `npm install express mysql2`
  * 2. Lancer le serveur avec `node api.js`
  * 3. Accéder à l'API via `http://localhost:3000/`
+ * 4. Créer/récupérer la clé de certificat en SSL
+ * 5. Mettre à jour le fichier .env avec les paramètres de la base de données MySQL mis à jour
  *
  * @date 24 février 2025
  */
