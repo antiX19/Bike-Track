86dd4973a34360b2ee6587f94f1593f62d663326
Your Name <dimanche9435@gmail.com>
2025-03-03 16:46:23 +0100

    Commit display

--- Frontend/app/src/main/java/com/exemple/applicationble/MainActivity.java
+++ Frontend/app/src/main/java/com/exemple/applicationble/MainActivity.java
@@ -6,8 +6,6 @@ import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.exemple.applicationble.R;
-
 public class MainActivity extends AppCompatActivity {
 
     private Button buttonVelo, buttonCommu, buttonConnexion;
@@ -15,20 +13,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test);
 
-        buttonVelo = findViewById(R.id.button_velo);
-        buttonCommu = findViewById(R.id.button_commu);
-        buttonConnexion = findViewById(R.id.button_connexion);
-
-        buttonVelo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Lancement de l'activité d'identification pour "J'ai un vélo"
-                Intent intent = new Intent(MainActivity.this, IdentificationActivity.class);
-                startActivity(intent);
-            }
-        });
+//        buttonVelo = findViewById(R.id.button_velo);
+//        buttonCommu = findViewById(R.id.button_commu);
+//        buttonConnexion = findViewById(R.id.button_connexion);
+//
+//        buttonVelo.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // Lancement de l'activité d'identification pour "J'ai un vélo"
+//                Intent intent = new Intent(MainActivity.this, IdentificationActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
         // Autres listeners pour les autres boutons...
     }
